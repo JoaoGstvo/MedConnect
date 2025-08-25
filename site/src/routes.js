72d063ea1a792e4365104c40e47b3254d4b3c4cc
Thiago SIlva <thiagosilva58469@gmail.com>
2025-08-25 09:58:48 -0300

    rotas de perfil adicionadas

--- site/src/routes.js
+++ site/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login/index.js';
 import LandingPage from './Pages/LandingPage/index.js'
 import Signup from './Pages/Cadastro/index.js';
+import PerfilEmpresa from './Pages/PerfilEmpresa/index.js'
+import PerfilFunc from './Pages/PerfilFunc/index.js'
 
 export default function Index() {
     return (
@@ -11,6 +13,8 @@ export default function Index() {
                 <Route path="/" element={<LandingPage/>} />
                 <Route path="/login" element={<Login/>} />
                 <Route path="/cadastro" element={<Signup/>} />
+                <Route path="/PerfilEmpresa" element={<PerfilEmpresa/>} />
+                <Route path="/PerfilFunc" element={<PerfilFunc/>} />
             </Routes>
         </BrowserRouter>
     )
