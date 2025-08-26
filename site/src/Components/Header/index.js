cd8e6cf5a15a94a99e61579147961ddfb66780bb
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-08-26 13:10:14 -0300

    teste

--- site/src/Components/Header/index.js
+++ site/src/Components/Header/index.js
@@ -25,7 +25,7 @@ function Header() {
                     </div>
 
                     <div className='nav-links-group profile'>
-                        <a href="/" className='nav-link a-profile'>
+                        <a href="/perfilprofissional" className='nav-link a-profile'>
                             <span>Perfil</span>
                         </a>
                     </div>
