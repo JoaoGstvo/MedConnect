63ae4f76eb838888507eca323aaff63df19c3a76
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-09-04 12:22:48 -0300

    Voltando Commit

--- site/src/routes.js
+++ site/src/routes.js
@@ -5,8 +5,6 @@ import LandingPage from './Pages/LandingPage/index.js'
 import Signup from './Pages/Cadastro/index.js';
 import BusnessProfile from './Pages/PerfilEmpresa/index.js';
 import ProfissionalProfile from './Pages/PerfilProfissional/index.js';
-import ArtigosPage from './Pages/Artigos/index.js';
-import CriarArtigoPage from './Pages/Artigos/Criar.js';
 
 
 export default function Index() {
@@ -18,8 +16,6 @@ export default function Index() {
                 <Route path="/login" element={<Login/>} />
                 <Route path="/cadastro" element={<Signup/>} />
                 <Route path="/perfilprofissional" element={<ProfissionalProfile/>} />
-                <Route path="/Artigos" element={<ArtigosPage/>} />
-                <Route path="Criar/" element={<CriarArtigoPage/>} />
             </Routes>
         </BrowserRouter>
     )
