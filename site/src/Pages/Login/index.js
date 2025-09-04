68060d768c851884b36acf02499ba5df69c50e6b
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-09-04 12:30:50 -0300

    Login - Escolhas

--- site/src/Pages/Login/index.js
+++ site/src/Pages/Login/index.js
@@ -1,47 +1,75 @@
 import './index.scss';
+import { useState } from 'react';
 // import BiometricLogin from '../../Components/Biometria';
 
 function Login() {
-    return (
-        <main className="login-page">
-            <section className='form-container'>
-                <div className='header'>
-                    <img src="/Images/Logo.png" alt="" />
-                </div>
-                {/* <BiometricLogin /> */}
-                <div className='divider'></div>
+  const [userType, setUserType] = useState('profissional'); // 'profissional' ou 'empresa'
 
-                <div className='form'>
-                    <h2>Fazer Login</h2>
-                    <p className='form-description'>Digite suas credenciais para acessar sua conta</p>
+  return (
+    <main className="login-page">
+      <section className='form-container'>
+        <div className='header'>
+          <img src="/Images/Logo.png" alt="" />
+        </div>
+        
+        {/* Seletor de tipo de usuário */}
+        <div className='user-type-selector'>
+          <button 
+            className={userType === 'profissional' ? 'active' : ''}
+            onClick={() => setUserType('profissional')}
+          >
+            Profissional
+          </button>
+          <button 
+            className={userType === 'empresa' ? 'active' : ''}
+            onClick={() => setUserType('empresa')}
+          >
+            Empresa
+          </button>
+        </div>
 
-                    <label className='input-field'>
-                        <span>E-mail</span>
-                        <input type="email" placeholder='seu@email.com' />
-                    </label>
+        {/* <BiometricLogin /> */}
+        <div className='divider'></div>
 
-                    <label className='input-field'>
-                        <span>Senha</span>
-                        <input type="password" placeholder='Sua senha' />
-                    </label>
+        <div className='form'>
+          <h2>Fazer Login como {userType === 'profissional' ? 'Profissional' : 'Empresa'}</h2>
+          <p className='form-description'>Digite suas credenciais para acessar sua conta</p>
 
-                    <div className='remember-me'>
-                        <label>
-                            <input type="checkbox" />
-                            <span>Lembrar de mim</span>
-                        </label>
-                        <a href="/" className='forgot-password'>Esqueceu a senha?</a>
-                    </div>
+          <label className='input-field'>
+            <span>E-mail</span>
+            <input type="email" placeholder='seu@email.com' />
+          </label>
 
-                    <button className='login-button'>Entrar</button>
+          <label className='input-field'>
+            <span>Senha</span>
+            <input type="password" placeholder='Sua senha' />
+          </label>
 
-                    <div className="signup-redirect">
-                        Não tem uma conta? <a href="/">Cadastre-se</a>
-                    </div>
-                </div>
-            </section>
-        </main>
-    );
+          {/* Campo adicional para empresas */}
+          {userType === 'empresa' && (
+            <label className='input-field'>
+              <span>CNPJ</span>
+              <input type="text" placeholder='00.000.000/0000-00' />
+            </label>
+          )}
+
+          <div className='remember-me'>
+            <label>
+              <input type="checkbox" />
+              <span>Lembrar de mim</span>
+            </label>
+            <a href="/" className='forgot-password'>Esqueceu a senha?</a>
+          </div>
+
+          <button className='login-button'>Entrar</button>
+
+          <div className="signup-redirect">
+            Não tem uma conta? <a href="/">Cadastre-se como {userType === 'profissional' ? 'Profissional' : 'Empresa'}</a>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Login;
