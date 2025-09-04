592da7f9df3f88fad5a55d33aaab028113229c21
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-09-04 13:29:34 -0300

    Detalhe

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
-import pool from './src/Repository/connection.js';
+// import pool from './src/Repository/connection.js';
 
 
 // To criando o servidor Express
